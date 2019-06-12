4e8ba3651179acc5869f41136847785b517ec27e
Alessia Bellisario <alessia@breather.com>
2019-06-11 20:26:38 -0400

    [sketch.js] 2019.06.11-20.26.38

--- sketch.js
+++ sketch.js
@@ -1,26 +1,37 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(2, 6);
+  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
+  console.log(palette);
+
   const createGrid = () => {
     const points = [];
-    const count = 40;
+    const count = 30;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([u, v]);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.1;
+        points.push({
+          color: random.pick(palette),
+          radius,
+          rotation: random.noise2D(u, v),
+          position: [u, v],
+        });
       }
     }
     return points;
   };
 
-  random.setSeed('512');
+  // random.setSeed('512');
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
@@ -28,15 +39,27 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach(data => {
+      const { position, radius, color, rotation } = data;
+      const [u, v] = position;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
-      context.strokeStyle = 'black';
-      context.lineWidth = 10;
-      context.stroke();
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.strokeStyle = 'black';
+      // context.lineWidth = 10;
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
+      context.fillStyle = color;
+      context.font = `${radius * width}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('—', 0, 0);
+      context.restore();
     });
   };
 };
