e66abdc2c4b0dd31f32370d6468758a1d13190fb
Alessia Bellisario <github@bellisar.io>
2020-11-13 14:01:05 -0500

    Heat map + binary tree algo

--- sketches/mazes/maze1.js
+++ sketches/mazes/maze1.js
@@ -6,17 +6,20 @@
  */
 
 const canvasSketch = require("canvas-sketch");
-const { polylinesToSVG, pathsToSVG } = require("canvas-sketch-util/penplot");
-// const pickRandom = require("canvas-sketch-util/random");
-// const palettes = require("nice-color-palettes");
+const { pathsToSVG } = require("canvas-sketch-util/penplot");
+const pickRandom = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
 
 const settings = {
-  dimensions: [25, 25],
+  dimensions: [5, 7],
   orientation: "portrait",
   pixelsPerInch: 300,
   scaleToView: true,
   units: "in",
 };
+
+const random = (min, max) => Math.random() * (max - min) + min;
+const palette = pickRandom.pick(palettes);
 
 const lines = [];
 
@@ -32,21 +35,21 @@ class Cell {
   }
 
   link(cell, bidi = true) {
-    this.links[cell.row + "#" + cell.column] = true;
+    this.links[cell.get_id()] = cell;
     if (bidi) cell.link(this, false);
   }
 
   unlink(cell, bidi = true) {
-    delete this.links[cell.row + "#" + cell.column];
+    delete this.links[cell.get_id()];
     if (bidi) cell.unlink(this, false);
   }
 
-  links() {
+  get_links() {
     return Object.keys(this.links);
   }
 
   isLinked(cell) {
-    return this.links.hasOwnProperty(cell.row + "#" + cell.column);
+    return this.links.hasOwnProperty(cell.get_id());
   }
 
   neighbors() {
@@ -57,6 +60,34 @@ class Cell {
     if (this.west) list.push(this.west);
     return list;
   }
+
+  get_id() {
+    return this.row + "#" + this.column;
+  }
+
+  distances() {
+    let distances = new Distances(this);
+    let frontier = [this];
+
+    while (frontier.length > 0) {
+      let new_frontier = [];
+
+      for (let i = 0; i < frontier.length; i += 1) {
+        let cell = frontier[i];
+        for (let link in cell.links) {
+          let linkedCell = cell.links[link];
+          if (linkedCell && distances.get_cell(linkedCell) == undefined) {
+            distances.set_cell(linkedCell, distances.get_cell(cell) + 1);
+            new_frontier.push(linkedCell);
+          }
+        }
+      }
+
+      frontier = new_frontier;
+    }
+
+    return distances;
+  }
 }
 
 class Grid {
@@ -66,6 +97,20 @@ class Grid {
 
     this.prepare_grid();
     this.configure_cells();
+  }
+
+  set_distances(distances) {
+    this.distances = distances;
+    let farthest_id;
+    [farthest_id, this.maximum] = distances.max();
+  }
+
+  background_color_for(cell) {
+    let distance = this.distances.get_cell(cell);
+    let intensity = ((this.maximum - distance) * 1.0) / this.maximum;
+    let dark = Math.floor(255 * intensity);
+    let bright = Math.floor(2 + 2 * intensity);
+    return `rgb(${dark},${bright},${dark})`;
   }
 
   prepare_grid() {
@@ -126,6 +171,10 @@ class Grid {
     }
   }
 
+  contents_of(cell) {
+    return " ";
+  }
+
   toString() {
     let output = "";
     output += "+" + "---+".repeat(this.columns) + "\n";
@@ -157,121 +206,175 @@ class Grid {
     return output;
   }
 
-  to_img(ctx, cellSize) {
-    ctx.strokeStyle = "red";
+  toImg(ctx, cellSize) {
+    ctx.strokeStyle = palette[0];
 
     let cell_gen = this.each_cell();
     while (true) {
       let cell = cell_gen.next().value;
       if (!cell) break;
 
-      let marginLeft = 1;
-      let marginTop = 1;
-      let x1 = cell.column * cellSize + marginLeft;
-      let y1 = cell.row * cellSize + marginTop;
-      let x2 = (cell.column + 1) * cellSize + marginLeft;
-      let y2 = (cell.row + 1) * cellSize + marginTop;
+      let margin = 0.05;
+      let x1 = cell.column * cellSize + margin;
+      let y1 = cell.row * cellSize + margin;
+      let x2 = (cell.column + 1) * cellSize + margin;
+      let y2 = (cell.row + 1) * cellSize + margin;
 
+      // console.log(this.background_color_for(cell));
+      // ctx.beginPath();
+      // ctx.rect(x1, y1, x2, y2);
+      // ctx.stroke();
+      // ctx.fillStyle = this.background_color_for(cell);
+      // ctx.fillRect(x1, y1, x2, y2);
 
       if (!cell.north) {
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y1);
         ctx.stroke();
-        lines.push([[x1, y1], [x2, y1]]);
+        lines.push([
+          [x1, y1],
+          [x2, y1],
+        ]);
       }
       if (!cell.west) {
         ctx.moveTo(x1, y1);
         ctx.lineTo(x1, y2);
         ctx.stroke();
-        lines.push([[x1, y1], [x1, y2]]);
+        lines.push([
+          [x1, y1],
+          [x1, y2],
+        ]);
       }
       if ((cell.east && !cell.isLinked(cell.east)) || !cell.east) {
         ctx.moveTo(x2, y1);
         ctx.lineTo(x2, y2);
         ctx.stroke();
-        lines.push([[x2, y1], [x2, y2]]);
+        lines.push([
+          [x2, y1],
+          [x2, y2],
+        ]);
       }
       if ((cell.south && !cell.isLinked(cell.south)) || !cell.south) {
         ctx.moveTo(x1, y2);
         ctx.lineTo(x2, y2);
         ctx.stroke();
-        lines.push([[x1, y2], [x2, y2]]);
+        lines.push([
+          [x1, y2],
+          [x2, y2],
+        ]);
       }
+    }
+  }
+  deadends() {
+    let list = [];
+
+    let cell_gen = this.each_cell();
+    while (true) {
+      let cell = cell_gen.next().value;
+      if (!cell) break;
+      if (cell.get_links().length == 1) {
+        list.push(cell);
+      }
+    }
+
+    return list;
+  }
+}
+
+class Distances {
+  constructor(root) {
+    this.root = root;
+    this.cells = {};
+    this.cells[this.root.get_id()] = 0;
+  }
+
+  get_cell(cell) {
+    return this.cells[cell.get_id()];
+  }
+
+  set_cell(cell, distance) {
+    this.cells[cell.get_id()] = distance;
+  }
+
+  get_cells() {
+    return Object.keys(this.cells);
+  }
+
+  path_to(goal) {
+    let current = goal;
+
+    let breadcrumbs = new Distances(this.root);
+    breadcrumbs.set_cell(current, this.get_cell(current));
+
+    while (current.get_id() !== this.root.get_id()) {
+      for (let link in current.links) {
+        let neighbor = current.links[link];
+        if (this.get_cell(neighbor) < this.get_cell(current)) {
+          breadcrumbs.set_cell(neighbor, this.get_cell(neighbor));
+          current = neighbor;
+          break;
+        }
+      }
+    }
+
+    return breadcrumbs;
+  }
+
+  max() {
+    let max_distance = 0;
+    let max_cell_id = this.root.get_id();
+
+    for (let cell_id in this.cells) {
+      let distance = this.cells[cell_id];
+      if (distance > max_distance) {
+        max_cell_id = cell_id;
+        max_distance = distance;
+      }
+    }
+
+    return [max_cell_id, max_distance];
+  }
+}
+
+class BinaryTree {
+  on(grid) {
+    let cell_gen = grid.each_cell();
+    while (true) {
+      let cell = cell_gen.next().value;
+      if (!cell) break;
+
+      let neighbors = [];
+      if (cell.north) neighbors.push(cell.north);
+      if (cell.east) neighbors.push(cell.east);
+
+      let index = Math.floor(Math.random() * neighbors.length);
+      let neighbor = neighbors[index];
+
+      if (neighbor) cell.link(neighbor);
     }
   }
 }
 
-
-class BinaryTree {
-	on(grid) {
-		let cell_gen = grid.each_cell()
-		while (true) {
-			let cell = cell_gen.next().value
-			if (!cell) break
-
-			let neighbors = []
-			if (cell.north) neighbors.push(cell.north)
-			if (cell.east) neighbors.push(cell.east)
-
-			let index = Math.floor(Math.random() * neighbors.length)
-			let neighbor = neighbors[index]
-
-			if (neighbor) cell.link(neighbor)
-		}
-	}
-}
-
-// function to generate a random number between min and max
-// const random = (min, max) => Math.random() * (max - min) + min;
-// const palette = pickRandom.pick(palettes);
-
 const sketch = (context) => {
-  // let marginBetweenElements = 0.05;
-  // let elementWidth = 0.25;
-  // let elementHeight = 0.25;
-  // position drawing in center of page
-  // let drawingWidth =
-  //   columns * (elementWidth + marginBetweenElements) - marginBetweenElements;
-  // let drawingHeight =
-  //   rows * (elementHeight + marginBetweenElements) - marginBetweenElements;
-  // let marginPageLeft = (context.width - drawingWidth) / 2;
-  // let marginPageTop = (context.height - drawingHeight) / 2;
-
   var maze = new BinaryTree();
-  let grid = new Grid(20, 20);
+  let grid = new Grid(45, 32);
 
   maze.on(grid);
 
-  console.log(grid.toString())
+  console.log(grid.toString());
+  let start = grid.get_cell(
+    Math.floor(grid.rows / 2),
+    Math.floor(grid.columns / 2)
+  );
+
+  grid.set_distances(start.distances());
 
   return ({ context, width, height, units }) => {
-    // black background
-    // context.strokeStyle = rainbow[pos];
-    context.lineWidth = 0.2;
-    context.lineCap = "square";
+    context.lineWidth = 0.015;
+    context.lineCap = "round";
     // context.lineJoin = "miter";
-    grid.to_img(context, 1);
+    grid.toImg(context, 0.15);
 
-    console.log(width, height, units)
-    // context.fillStyle = "black";
-    // context.fillRect(0, 0, width, height);
-    // let posX = marginPageLeft;
-    // let posY = marginPageTop;
-    // for (let r = 0; r < rows; r++) {
-    //   for (let i = 0; i < columns; i++) {
-    //     drawSquare(
-    //       context,
-    //       posX,
-    //       posY + o[r][i].move,
-    //       elementWidth,
-    //       o[r][i].angle,
-    //       [r, i]
-    //     );
-    //     posX = posX + elementWidth + marginBetweenElements;
-    //   }
-    //   posX = marginPageLeft;
-    //   posY = posY + elementHeight + marginBetweenElements;
-    // }
     return [
       context.canvas,
       {
@@ -283,17 +386,6 @@ const sketch = (context) => {
         extension: ".svg",
       },
     ];
-    // return [
-    //   context.canvas,
-    //   {
-    //     data: polylinesToSVG(lines, {
-    //       width,
-    //       height,
-    //       units,
-    //     }),
-    //     extension: ".svg",
-    //   },
-    // ];
   };
 };
 
